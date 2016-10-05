e958a6077eebe0be001ab6e50bc0b83f447e748c
Vermillion <tjc0609@gmail.com>
2016-10-04 21:40:44 -0700

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Tracking System</div>
+
+                <div class="panel-body">
+
+                    <form action="" method="POST" role="form">
+                        <legend>Time Worked</legend>
+
+                        <div class="form-group">
+                            <label for="">label</label>
+                            <input type="text" class="form-control" id="" placeholder="Input field">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    Basic panel example
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
