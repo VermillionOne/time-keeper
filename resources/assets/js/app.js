36605149e64d8ad1d8f187620ec18cdb80384a17
Vermillion <tjc0609@gmail.com>
2016-10-16 18:39:07 -0700

    Retooling Projects object as well as time and assignments

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,54 +18,162 @@ Vue.component('example', require('./components/Example.vue'));
 var app = new Vue({
     el: 'body',
     data: {
-        projectTitle: '',
-        startTime: '',
-        endTime: '',
-        times: [],
+        projectTitle    : '',
+        assignmentTitle : '',
+        startTime       : '',
+        endTime         : '',
+        editIndex       : '',
+        editable        : false,
+        assignments     : [],
+        projects        : [],
+        projArr         : []
+    },
+    computed: {
+        start : function () {
+            result = this.startTime.split(':');
+            return result;
+        },
+        end : function () {
+            result = this.endTime.split(':');
+            return result;
+        },
+        startHours : function () {
+            result = parseInt(this.start[0]);
+            return result;
+        },
+        startMins : function () {
+            result = parseInt(this.start[1]);
+            return result;
+        },
+        endHours : function () {
+            result = parseInt(this.end[0]);
+            return result;
+        },
+        endMins : function () {
+            result = parseInt(this.end[1]);
+            return result;
+        },
+        startTotal : function () {
+            result = this.startHours*60 + this.startMins;
+            return result;
+        },
+        endTotal : function () {
+            result = this.endHours*60 + this.endMins;
+            return result;
+        },
+        timeDiff : function () {
+            result = this.getTimeDifference();
+            return result;
+        },
+        minsWorked : function () {
+            result = this.getMinutesWorked();
+            return result;
+        },
+        hoursWorked : function () {
+            result = this.getHoursWorked();
+            return result;
+        },
+        elapsedTime : function () {
+            result = this.getActualTimeElapsed();
+            return result;
+        }
     },
     methods: {
-        addTime: function () {
-            console.log(this.times);
-            var start         = this.startTime.split(':'),
-                end           = this.endTime.split(':'),
-                startHours    = parseInt(start[0]),
-                startMins     = parseInt(start[1]),
-                endHours      = parseInt(end[0]),
-                endMins       = parseInt(end[1]),
-                startTotal    = startHours*60 + startMins,
-                endTotal      = endHours*60 + endMins,
-                timeDiff      = getTimeDifference(),
-                minsWorked    = getMinutesWorked(),
-                hoursWorked   = getHoursWorked(),
-                actualElapsed = getActualTimeElapsed();
+        addAssignment: function () {
+            var title = this.assignmentTitle.trim();
+            if (title) {
+                console.log(title);
+                console.log(this.assignments);
+                this.assignments.push({title: title});
+                this.assignmentTitle = '';
 
-            function getTimeDifference() {
-                var diff = endTotal - startTotal;
-                console.log(diff);
-                if ( diff < 0) {
-                    diff = (24*60) + diff;
-                }
-                return diff;
             }
-            function getMinutesWorked() {
-                var mins = timeDiff % 60;
-                return mins;
-            }
-            function getHoursWorked() {
-                var hours = (timeDiff - minsWorked) / 60;
-                return hours;
-            }
-            function getActualTimeElapsed() {
-                var time = hoursWorked + ":" + minsWorked;
-                return time;
-            }
-
-            var elapsed = actualElapsed.trim();
-            this.times.push({workedTime: elapsed});
-            console.log(this.times);
         },
-        addProjectName: function () {
-            this.projectTitle.splice();
+        addProject: function () {
+            var projectTitle = this.projectTitle.trim();
+            if (projectTitle) {
+                console.log(projectTitle);
+                this.projects.push({title: projectTitle})
+                this.projectTitle = '';
+            }
+        },
+        addtime: function () {
+
+            console.log(this.times);
+
+            /**
+             * Adding time instances to the times array
+             */
+
+            // var times = getElapsedTime();
+
+            // this.times.push();
+            // this.assignments.title = assignmentTitle;
+            // this.assignments.times = this.times;
+            // this.project.title = projectTitle;
+            // this.project.assignments = this.assignments;
+
+            // this.projArr.push(this.project);
+
+            // console.log(this.times);
+            // this.startTime = '';
+            // this.endTime = '';
+            // this.editable = false;
+        },
+        addProjectTitle: function () {
+            var title = projectTitle.trim();
+            if (title) {
+                this.projects.push({title: title});
+                this.projectTitle = '';
+            }
+        },
+        updateTime: function () {
+
+            var index = this.editIndex;
+
+            this.times.$set( index,
+                {
+                    start: this.startTime,
+                    end: this.endTime,
+                    workedTime: elapsed
+                }
+            );
+        },
+        removeTime: function (index) {
+            console.log(index);
+            var i = this.times[index];
+            this.times.$remove(i);
+        },
+        editTime: function (index) {
+            console.log(index);
+            this.startTime = this.times[index].start;
+            this.endTime = this.times[index].end;
+            this.editIndex = index;
+            this.editable = true;
+        },
+        getElapsedTime: function () {
+            var elapsed = actualElapsed.trim();
+            return elapsed;
+        },
+        getTimeDifference: function () {
+            var diff = endTotal - startTotal;
+            console.log(diff);
+            if ( diff < 0) {
+                diff = (24*60) + diff;
+            }
+            return diff;
+        },
+        getMinutesWorked: function () {
+            var mins = timeDiff % 60;
+            return mins;
+        },
+        getHoursWorked: function () {
+            var hours = (timeDiff - minsWorked) / 60;
+            return hours;
+        },
+        getActualTimeElapsed: function () {
+            var time = hoursWorked + ":" + minsWorked;
+            return time;
         }
     }
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,14 +14,29 @@
 
                         <div class="form-group">
                             <label for="">Project Title</label>
-                            <input type="text" v-model="projectTitle" class="form-control" placeholder="Give your project a clever name, like Gobias Industries, but not that...">
+                            <div class="input-group">
+                                <input type="text" v-model="projectTitle" class="form-control" placeholder="Give your project a clever name, like Gobias Industries, but not that...">
+                                <span class="input-group-btn">
+                                    <button v-on:click="addProject" class="btn btn-primary">Add Project</button>
+                                </span>
+                            </div>
+
                         </div>
 
-                        <label>Add a time frame of work:</label>
+                        {{-- <div class="form-group"> --}}
+                            {{-- <label for="">Assignment Name</label> --}}
+                            {{-- <div class="input-group"> --}}
+                                {{-- <input type="text" v-model="assignmentTitle" class="form-control" placeholder="Name your assignment."> --}}
+                               {{--  <span class="input-group-btn">
+                                    <button class="btn btn-primary" v-on:click="addAssignment" type="button">Add Assignment</button>
+                                </span> --}}
+                            {{-- </div> --}}
+                        {{-- </div> --}}
+
+                        {{-- <label>Add a time frame of work:</label> --}}
 
                     </div>
-
-                    <div  method="POST" class="form-inline" role="form">
+                    {{--  <div class="form-inline" role="form">
 
                         <div class="form-group">
                             <label for="">From </label>
@@ -33,28 +48,68 @@
                             <input type="time" name="endTime" v-model="endTime" class="form-control" id="end-time" placeholder="Input field">
                         </div>
 
-                        <button type="submit" v-on:click="addTime" class="btn btn-primary">Submit</button>
+                        <button type="submit" v-on:click="addTime" v-if="editable != true" class="btn btn-primary">Submit</button>
+                        <button type="submit" v-on:click="addTime" v-if="editable == true" class="btn btn-primary">Update</button>
 
-                    </div>
+                    </div> --}}
 
                 </div>
             </section>
 
+            <div v-for="project in projects" class="panel panel-default">
 
-            <div v-if="projectTitle || startTime" class="panel panel-default">
-
-                <div class="panel-heading" v-if="projectTitle">
-                    <h3 class="panel-title">@{{projectTitle}}</h3>
+                <div class="panel-heading">
+                    <h3 class="panel-title">@{{project.title}}</h3>
                 </div>
+
 
                 <div class="panel-body">
 
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" v-model="assignmentTitle" v-on:keyup.enter="addAssignmentTitle" class="form-control" placeholder="Give your project a clever name, like Gobias Industries, but not that...">
+                            <span class="input-group-btn">
+                                <button v-on:click="addProject" class="btn btn-primary">Add Assignment</button>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="form-inline" role="form">
+
+                        <div class="form-group">
+                            <label for="">From </label>
+                            <input type="time" name="startTime" v-model="startTime" class="form-control" id="start-time" placeholder="Input field">
+                        </div>
+
+                        <div class="form-group">
+                            <label for=""> Until </label>
+                            <input type="time" name="endTime" v-model="endTime" class="form-control" id="end-time" placeholder="Input field">
+                        </div>
+
+                        <button type="submit" v-on:click="addTime" v-if="editable != true" class="btn btn-primary">Submit</button>
+                        <button type="submit" v-on:click="addTime" v-if="editable == true" class="btn btn-primary">Update</button>
+
+                    </div>
+
                     <ul class="list-group">
-                        <li v-for="time in times" class="list-group-item">@{{time.workedTime}} Hours</li>
+
+                        <li v-for="assignment in assignments" name="item-@{{$index}}" class="list-group-item">
+
+                            <li class="list-group-item" >@{{assignment.title}}</li>
+                            <ul class="list-group">
+
+                                <li v-for="time in assignment.times" class="list-group-item">@{{time.workedTime}} Hours
+                                    <span class="badge" v-on:click="removeTime($index)"><span class="glyphicon glyphicon-remove"></span></span>
+                                    <span class="badge" v-on:click="editTime($index)"><span class="glyphicon glyphicon-pencil"></span></span>
+                                </li>
+                            </ul>
+
+                        </li>
                     </ul>
 
                 </div>
             </div>
+            {{-- End Projects v-for --}}
         </div>
     </div>
 </div>
